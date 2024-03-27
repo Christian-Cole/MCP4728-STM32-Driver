0363845f708f476883178fca06a8049c42976776
cole-ch <cole.ch@northeastern.edu>
2024-03-27 16:22:02 -0400

    Lots of driver rewrites and updates, as well as the addition of EEPROM and non-EEPROM writing function calls

--- src/drivers-peripheral/MCP4728-i2c-driver.cpp
+++ src/drivers-peripheral/MCP4728-i2c-driver.cpp
@@ -1,88 +1,291 @@
 #include "MCP4728-i2c-driver.h"
 
+#include "stm32f103xb.h"
 #include "stm32f1xx_hal_def.h"
+#include "stm32f1xx_hal_gpio.h"
 #include "stm32f1xx_hal_i2c.h"
+#include <stdint.h>
 
 
-LP50xx::LP50xx(uint8_t _i2c_address, I2C_HandleTypeDef *_i2c_handle)
+MCP4728::MCP4728(uint8_t _i2c_address, I2C_HandleTypeDef *_i2c_handle, GPIO_TypeDef *_LDAC_pinPort, uint16_t _LDAC_pin, GPIO_TypeDef *_RDY_pinPort, uint16_t _RDY_pin)
 {
     i2c_handle = _i2c_handle;
     i2c_address = _i2c_address;
+    LDAC_pinPort = _LDAC_pinPort;
+    LDAC_pin = _LDAC_pin;
+    RDY_pinPort = _RDY_pinPort;
+    RDY_pin = _RDY_pin;
 }
 
 
 
-
-void LP50xx::enable()
+void MCP4728::singleWrite(uint8_t channelSelect, uint8_t channelUpdate, uint8_t referenceUpdate, uint8_t powerDownUpdate, uint8_t gainUpdate, uint16_t outputValue)
 {
-    data_to_send = CHIP_EN;
-    HAL_I2C_Mem_Write(i2c_handle, i2c_address, DEVICE_CONFIG0, I2C_MEMADD_SIZE_8BIT, &data_to_send, 1, HAL_MAX_DELAY);
+    uint8_t data_to_send[3];
+    data_to_send[0] = MULTI_WRITE | ((channelSelect << 1) & 0x06) | (channelUpdate & 0x01);
+    data_to_send[1] = ((referenceUpdate << 7) & 0x80) | ((powerDownUpdate << 5) & 0x60) | ((gainUpdate << 4) & 0x10) | ((outputValue >> 8) & 0x0F);
+    data_to_send[2] = outputValue & 0xFF;  
+    HAL_I2C_Master_Transmit(i2c_handle, i2c_address, data_to_send, 3, HAL_MAX_DELAY);
 }
 
-void LP50xx::config(uint8_t configValue)
+void MCP4728::singleWrite(uint8_t channelSelect, uint16_t outputValue)
 {
-    data_to_send = configValue;
-    HAL_I2C_Mem_Write(i2c_handle, i2c_address, DEVICE_CONFIG1, I2C_MEMADD_SIZE_8BIT, &data_to_send, 1, HAL_MAX_DELAY);
+    uint8_t data_to_send[3];
+    data_to_send[0] = MULTI_WRITE | ((channelSelect << 1) & 0x06);
+    data_to_send[1] = (outputValue >> 8) & 0x0F;
+    data_to_send[2] = outputValue & 0xFF;  
+    HAL_I2C_Master_Transmit(i2c_handle, i2c_address, data_to_send, 3, HAL_MAX_DELAY);
 }
 
-void LP50xx::reset()
+void MCP4728::singleWriteNoLoad(uint8_t channelSelect, uint16_t outputValue)
 {
-    data_to_send = RESET_VALUE;
-    HAL_I2C_Mem_Write(i2c_handle, i2c_address, RESET, I2C_MEMADD_SIZE_8BIT, &data_to_send, 1, HAL_MAX_DELAY);
+    uint8_t data_to_send[3];
+    data_to_send[0] = MULTI_WRITE | ((channelSelect << 1) & 0x06) | 0x01;
+    data_to_send[1] = (outputValue >> 8) & 0x0F;
+    data_to_send[2] = outputValue & 0xFF;  
+    HAL_I2C_Master_Transmit(i2c_handle, i2c_address, data_to_send, 3, HAL_MAX_DELAY);
 }
 
-
-
-
-void LP50xx::bankConfig(uint8_t bankConfigValue)
+void MCP4728::singleWriteEEPROM(uint8_t channelSelect, uint8_t channelUpdate, uint8_t referenceUpdate, uint8_t powerDownUpdate, uint8_t gainUpdate, uint16_t outputValue)
 {
-    data_to_send = bankConfigValue;
-    HAL_I2C_Mem_Write(i2c_handle, i2c_address, LED_CONFIG0, I2C_MEMADD_SIZE_8BIT, &data_to_send, 1, HAL_MAX_DELAY);
-}
-
-void LP50xx::setBankBrightness(uint8_t bankBrightnessValue)
-{
-    data_to_send = bankBrightnessValue;
-    HAL_I2C_Mem_Write(i2c_handle, i2c_address, BANK_BRIGHTNESS, I2C_MEMADD_SIZE_8BIT, &data_to_send, 1, HAL_MAX_DELAY);
-}
-
-void LP50xx::setBankColor(uint8_t bank, uint8_t bankColorValue)
-{
-    data_to_send = bankColorValue;
-    HAL_I2C_Mem_Write(i2c_handle, i2c_address, (BANK_A_COLOR + bank), I2C_MEMADD_SIZE_8BIT, &data_to_send, 1, HAL_MAX_DELAY);
+    uint8_t data_to_send[3];
+    data_to_send[0] = SINGLE_WRITE | ((channelSelect & 0x03) << 1) | (channelUpdate & 0x01); 
+    data_to_send[1] = ((referenceUpdate << 7) & 0x80) | ((powerDownUpdate << 5) & 0x60) | ((gainUpdate << 4) & 0x10) | ((outputValue >> 8) & 0x0F);
+    data_to_send[2] = outputValue & 0xFF; 
+    HAL_I2C_Master_Transmit(i2c_handle, i2c_address, data_to_send, 3, HAL_MAX_DELAY);
 }
 
 
 
-void LP50xx::setLEDBrightness(uint8_t LED, uint8_t LEDBrightnessValue)
+void MCP4728::multiwrite(uint8_t numUpdates, uint8_t* channelSelect, uint8_t* channelUpdates, uint8_t* referenceUpdates, uint8_t* powerDownUpdates, uint8_t* gainUpdates, uint16_t* outputValues)
 {
-    data_to_send = LEDBrightnessValue;
-    HAL_I2C_Mem_Write(i2c_handle, i2c_address, (LED0_BRIGHTNESS + LED), I2C_MEMADD_SIZE_8BIT, &data_to_send, 1, HAL_MAX_DELAY);
+    if (numUpdates > 4)
+    {
+        return;
+    }
+
+    uint8_t data_to_send[numUpdates * 3];
+    for (int i = 0; i < numUpdates; i++)
+    {
+        data_to_send[(i * 3) + 0] = MULTI_WRITE | ((channelSelect[i] << 1) & 0x06) | (channelUpdates[i] & 0x01);
+        data_to_send[(i * 3) + 1] = ((referenceUpdates[i] << 7) & 0x80) | ((powerDownUpdates[i] << 5) & 0x60) | ((gainUpdates[i] << 4) & 0x10) | ((outputValues[i] >> 8) & 0x0F);
+        data_to_send[(i * 3) + 2] = outputValues[i] & 0xFF; 
+    }
+
+    uint8_t bytesTransfered = numUpdates * 3;
+
+    HAL_I2C_Master_Transmit(i2c_handle, i2c_address, data_to_send, bytesTransfered, HAL_MAX_DELAY);
 }
 
-void LP50xx::setOutColor(uint8_t out, uint8_t outColorValue)
+void MCP4728::multiwrite(uint8_t numUpdates, uint8_t* channelSelect, uint16_t* outputValues)
 {
-    data_to_send = outColorValue;
-    HAL_I2C_Mem_Write(i2c_handle, i2c_address, (OUT0_COLOR + out), I2C_MEMADD_SIZE_8BIT, &data_to_send, 1, HAL_MAX_DELAY);
+    if (numUpdates > 4)
+    {
+        return;
+    }
+
+    uint8_t data_to_send[numUpdates * 3];
+    for (int i = 0; i < numUpdates; i++)
+    {
+        data_to_send[(i * 3) + 0] = MULTI_WRITE | ((channelSelect[i] << 1) & 0x06);
+        data_to_send[(i * 3) + 1] = (outputValues[i] >> 8) & 0x0F;
+        data_to_send[(i * 3) + 2] = outputValues[i] & 0xFF; 
+    }
+
+    uint8_t bytesTransfered = numUpdates * 3;
+
+    HAL_I2C_Master_Transmit(i2c_handle, i2c_address, data_to_send, bytesTransfered, HAL_MAX_DELAY);
 }
 
-void LP50xx::setRGBColor(uint8_t LED, uint8_t valueR, uint8_t valueG, uint8_t valueB)
+
+
+void MCP4728::sequentialWrite(uint8_t channelStart, uint8_t* channelUpdates, uint8_t* referenceUpdates, uint8_t* powerDownUpdates, uint8_t* gainUpdates, uint16_t* outputValues)
 {
-    uint8_t dataRGB[3] {valueR, valueG, valueB};
-    HAL_I2C_Mem_Write(i2c_handle, i2c_address, (OUT0_COLOR + (LED * 3)), I2C_MEMADD_SIZE_8BIT, dataRGB, 3, HAL_MAX_DELAY);
+    if (channelStart > 3)
+    {
+        return;
+    }
+    uint8_t channel = channelStart;
+
+    uint8_t data_to_send[(4 - channelStart) * 3];
+    for (int i = 0; i < (4 - channelStart); i++)
+    {
+        data_to_send[(i * 3) + 0] = MULTI_WRITE | ((channel << 1) & 0x06) | (channelUpdates[i] & 0x01);
+        data_to_send[(i * 3) + 1] = ((referenceUpdates[i] << 7) & 0x80) | ((powerDownUpdates[i] << 5) & 0x60) | ((gainUpdates[i] << 4) & 0x10) | ((outputValues[i] >> 8) & 0x0F);
+        data_to_send[(i * 3) + 2] = outputValues[i] & 0xFF;
+
+        channel++;
+    }
+
+    uint8_t bytesTransfered = (4 - channelStart) * 3;
+
+    HAL_I2C_Master_Transmit(i2c_handle, i2c_address, data_to_send, bytesTransfered, HAL_MAX_DELAY);
+}
+
+void MCP4728::sequentialWrite(uint8_t channelStart, uint16_t* outputValues)
+{
+    if (channelStart > 3)
+    {
+        return;
+    }
+    uint8_t channel = channelStart;
+
+    uint8_t data_to_send[(4 - channelStart) * 3];
+    for (int i = 0; i < (4 - channelStart); i++)
+    {
+        data_to_send[(i * 3) + 0] = MULTI_WRITE | ((channel << 1) & 0x06);
+        data_to_send[(i * 3) + 1] = (outputValues[i] >> 8) & 0x0F;
+        data_to_send[(i * 3) + 2] = outputValues[i] & 0xFF;
+
+        channel++;
+    }
+
+    uint8_t bytesTransfered = (4 - channelStart) * 3;
+
+    HAL_I2C_Master_Transmit(i2c_handle, i2c_address, data_to_send, bytesTransfered, HAL_MAX_DELAY);
+}
+
+void MCP4728::sequentialWriteEEPROM(uint8_t channelStart, uint8_t channelUpdate, uint8_t* referenceUpdates, uint8_t* powerDownUpdates, uint8_t* gainUpdates, uint16_t* outputValues)
+{
+    if (channelStart > 3)
+    {
+        return;
+    }
+    
+    uint8_t data_to_send[(2 * (4 - channelStart)) + 1];
+    data_to_send[0] = SEQ_WRITE | ((channelStart & 0x03) << 1) | (channelUpdate & 0x01); 
+    for (int i = 0; i < channelStart; i++)
+    {
+        data_to_send[(i * 2) + 1] = ((referenceUpdates[i] << 7) & 0x80) | ((powerDownUpdates[i] << 5) & 0x60) | ((gainUpdates[i] << 4) & 0x10) | ((outputValues[i] >> 8) & 0x0F);
+        data_to_send[(i * 2) + 2] = outputValues[i] & 0xFF; 
+    }
+
+    uint8_t bytesTransfered = (2 * (4 - channelStart)) + 1;
+
+    HAL_I2C_Master_Transmit(i2c_handle, i2c_address, data_to_send, bytesTransfered, HAL_MAX_DELAY);
 }
 
 
 
-void LP50xx::registerWrite(uint8_t reg, uint8_t value)
+void MCP4728::fastWrite(uint8_t* powerDownUpdates, uint16_t* outputValues, uint8_t numChannelWrites)
 {
-    data_to_send = value;
-    HAL_I2C_Mem_Write(i2c_handle, i2c_address, reg, I2C_MEMADD_SIZE_8BIT, &data_to_send, 1, HAL_MAX_DELAY);
+    if (numChannelWrites > 4)
+    {
+        numChannelWrites = 4;
+    }
+
+    uint8_t data_to_send[(numChannelWrites * 2)];
+    for (int i = 0; i < numChannelWrites; i++)
+    {
+        data_to_send[(i * 2)] = FAST_WRITE | ((powerDownUpdates[i] << 4) & 0x30) | ((outputValues[i] >> 8) & 0x0F);
+        data_to_send[(i * 2) + 1] = outputValues[i] & 0xFF;
+    }
+
+    uint8_t bytesTransfered = 2 * numChannelWrites;
+
+    HAL_I2C_Master_Transmit(i2c_handle, i2c_address, data_to_send, bytesTransfered, HAL_MAX_DELAY);
 }
 
-uint8_t LP50xx::registerRead(uint8_t reg)
+void MCP4728::fastWrite(uint16_t* outputValues, uint8_t numChannelWrites)
 {
-    uint8_t dataRead;
-    HAL_I2C_Mem_Read(i2c_handle, i2c_address, reg, I2C_MEMADD_SIZE_8BIT, &dataRead, 1, HAL_MAX_DELAY);
-    return dataRead;
+    if (numChannelWrites > 4)
+    {
+        numChannelWrites = 4;
+    }
+
+    uint8_t data_to_send[(numChannelWrites * 2)];
+    for (int i = 0; i < numChannelWrites; i++)
+    {
+        data_to_send[(i * 2)] = FAST_WRITE | ((outputValues[i] >> 8) & 0x0F);
+        data_to_send[(i * 2) + 1] = outputValues[i] & 0xFF;
+    }
+
+    uint8_t bytesTransfered = 2 * numChannelWrites;
+
+    HAL_I2C_Master_Transmit(i2c_handle, i2c_address, data_to_send, bytesTransfered, HAL_MAX_DELAY);
+}
+
+void MCP4728::fastWrite(uint16_t outputChannel1, uint16_t outputChannel2, uint16_t outputChannel3, uint16_t outputChannel4)
+{
+    uint8_t data_to_send[8];
+    
+    data_to_send[0] = FAST_WRITE | ((outputChannel1 >> 8) & 0x0F);
+    data_to_send[1] = outputChannel1 & 0xFF;
+    data_to_send[2] = FAST_WRITE | ((outputChannel2 >> 8) & 0x0F);
+    data_to_send[3] = outputChannel2 & 0xFF;
+    data_to_send[4] = FAST_WRITE | ((outputChannel3 >> 8) & 0x0F);
+    data_to_send[5] = outputChannel3 & 0xFF;
+    data_to_send[6] = FAST_WRITE | ((outputChannel4 >> 8) & 0x0F);
+    data_to_send[7] = outputChannel4 & 0xFF;
+
+    HAL_I2C_Master_Transmit(i2c_handle, i2c_address, data_to_send, 8, HAL_MAX_DELAY);
+}
+
+
+
+void MCP4728::writeAddressBits() //more complicated, required LDAC change in the middle of sending, DO NOT USE RN
+{
+    uint8_t data_to_send[3];
+    data_to_send[0] = ADDR_BITS_WRITE; //not done
+    data_to_send[1] = ADDR_BITS_WRITE; //not done
+    data_to_send[2] = ADDR_BITS_WRITE; //not done
+    HAL_I2C_Master_Transmit(i2c_handle, i2c_address, data_to_send, 3, HAL_MAX_DELAY);
+}
+
+void MCP4728::writeReferenceBits(uint8_t referenceConfig)
+{
+    uint8_t data_to_send = REF_BITS_WRITE | (referenceConfig & 0x0F); //masks bits 0-3 for referenceConfig to prevent it from affecting the command bits
+    HAL_I2C_Master_Transmit(i2c_handle, i2c_address, &data_to_send, 1, HAL_MAX_DELAY);
+}
+
+void MCP4728::writeGainBits(uint8_t gainConfig)
+{
+    uint8_t data_to_send = GAIN_BITS_WRITE | (gainConfig & 0x0F); //masks bits 0-3 for gainConfig to prevent it from affecting the command bits
+    HAL_I2C_Master_Transmit(i2c_handle, i2c_address, &data_to_send, 1, HAL_MAX_DELAY);
+}
+
+void MCP4728::writePowerDownBits(uint8_t powerDownConfig)
+{
+    uint8_t data_to_send[2];
+    data_to_send[0] = PWRDN_BITS_WRITE | ((powerDownConfig >> 4) & 0x0F); //first byte is the command bits and the first 4 bits of powerConfig data
+    data_to_send[1] = (powerDownConfig << 4) & 0xF0; //second byte is the last 4 bits of powerConfig data shifted to the left
+    HAL_I2C_Master_Transmit(i2c_handle, i2c_address, data_to_send, 2, HAL_MAX_DELAY);
+}
+
+
+
+void MCP4728::setLDAC(GPIO_PinState pinState)
+{
+    HAL_GPIO_WritePin(LDAC_pinPort, LDAC_pin, pinState);
+}
+
+GPIO_PinState MCP4728::getRDYState()
+{
+    return HAL_GPIO_ReadPin(RDY_pinPort, RDY_pin);
+}
+
+
+
+void MCP4728::genCall_reset()
+{
+    uint8_t data_to_send = GEN_CALL_RST;
+    HAL_I2C_Master_Transmit(i2c_handle, GEN_CALL_ADDR, &data_to_send, 1, HAL_MAX_DELAY);
+}
+
+void MCP4728::genCall_wakeUp()
+{
+    uint8_t data_to_send = GEN_CALL_WKUP;
+    HAL_I2C_Master_Transmit(i2c_handle, GEN_CALL_ADDR, &data_to_send, 1, HAL_MAX_DELAY);
+}
+
+void MCP4728::genCall_softwareUpdate()
+{
+    uint8_t data_to_send = GEN_CALL_WKUP;
+    HAL_I2C_Master_Transmit(i2c_handle, GEN_CALL_ADDR, &data_to_send, 1, HAL_MAX_DELAY);
+}
+
+void MCP4728::genCall_readAddrBits() //USES THE LDAC PIN TO INDICATE DEV ON INTEREST, THIS DOESNT WORK RN
+{
+    uint8_t data_to_send = GEN_CALL_RDADDR;
+    HAL_I2C_Master_Transmit(i2c_handle, GEN_CALL_ADDR, &data_to_send, 1, HAL_MAX_DELAY);
 }
--- src/drivers-peripheral/MCP4728-i2c-driver.h
+++ src/drivers-peripheral/MCP4728-i2c-driver.h
@@ -1,66 +1,81 @@
 #pragma once
-#ifndef LP50XX_I2C_DRIVER_H_
-#define LP50XX_I2C_DRIVER_H_
+#include "stm32f103xb.h"
+#include "stm32f1xx_hal_gpio.h"
+#ifndef MCP4728_I2C_DRIVER_H_
+#define MCP4728_I2C_DRIVER_H_
 
 #include "stm32f1xx_hal.h"
 #include <stdint.h>
 
-//register definitions
-#define DEVICE_CONFIG0  0x00
-#define DEVICE_CONFIG1  0x01
-#define LED_CONFIG0     0x02
-#define BANK_BRIGHTNESS 0x03
-#define BANK_A_COLOR    0x04
-#define BANK_B_COLOR    0x05
-#define BANK_C_COLOR    0x06
-#define LED0_BRIGHTNESS 0x07
-#define LED1_BRIGHTNESS 0x08
-#define LED2_BRIGHTNESS 0x09
-#define LED3_BRIGHTNESS 0x0A
-#define OUT0_COLOR      0x0B
-#define OUT1_COLOR      0x0C
-#define OUT2_COLOR      0x0D
-#define OUT3_COLOR      0x0E
-#define OUT4_COLOR      0x0F
-#define OUT5_COLOR      0x10
-#define OUT6_COLOR      0x11
-#define OUT7_COLOR      0x12
-#define OUT8_COLOR      0x13
-#define OUT9_COLOR      0x14
-#define OUT10_COLOR     0x15
-#define OUT11_COLOR     0x16
-#define RESET           0x17
+//address definitions (already left-justified for R/W bit)
+#define ADDR_A0 0b11000000
+#define ADDR_A1 0b11000010
+#define ADDR_A2 0b11000100
+#define ADDR_A3 0b11000110
+#define ADDR_A4 0b11001000
+#define ADDR_A5 0b11001010
+#define ADDR_A6 0b11001100
+#define ADDR_A7 0b11001110
 
-//important data values
-#define CHIP_EN         0x40
-#define CONFIG1_RST_VAL 0x3C
-#define RESET_VALUE     0xFF
+//general call definitions
+#define GEN_CALL_ADDR   0x00
+#define GEN_CALL_RST    0x06
+#define GEN_CALL_WKUP   0x09
+#define GEN_CALL_SWUP   0x08
+#define GEN_CALL_RDADDR 0x0C
 
-class LP50xx
+//command definitions (left justified to ease calculations with other bits)
+#define FAST_WRITE       0b00000000
+#define MULTI_WRITE      0b01000000
+#define SEQ_WRITE        0b01010000
+#define SINGLE_WRITE     0b01011000
+#define ADDR_BITS_WRITE  0b01100000
+#define REF_BITS_WRITE   0b10000000
+#define GAIN_BITS_WRITE  0b11000000
+#define PWRDN_BITS_WRITE 0b10100000
+
+class MCP4728
 {
 public:
-    LP50xx(uint8_t _i2c_address, I2C_HandleTypeDef *_i2c_handle);
+    MCP4728(uint8_t _i2c_address, I2C_HandleTypeDef *_i2c_handle, GPIO_TypeDef *_LDAC_pinPort, uint16_t _LDAC_pin, GPIO_TypeDef *_RDY_pinPort, uint16_t _RDY_pin);
 
-    void enable();
-    void config(uint8_t configValue);
-    void reset();
+    void singleWrite(uint8_t channelSelect, uint8_t channelUpdate, uint8_t referenceUpdate, uint8_t powerDownUpdate, uint8_t gainUpdate, uint16_t outputValue);
+    void singleWrite(uint8_t channelSelect, uint16_t outputValue);
+    void singleWriteNoLoad(uint8_t channelSelect, uint16_t outputValue);
+    void singleWriteEEPROM(uint8_t channelSelect, uint8_t channelUpdate, uint8_t referenceUpdate, uint8_t powerDownUpdate, uint8_t gainUpdate, uint16_t outputValue);
 
-    void bankConfig(uint8_t bankConfigValue);
-    void setBankBrightness(uint8_t bankBrightnessValue);
-    void setBankColor(uint8_t bank, uint8_t bankColorValue);
+    void multiwrite(uint8_t numUpdates, uint8_t* channelSelect, uint8_t* channelUpdates, uint8_t* referenceUpdates, uint8_t* powerDownUpdates, uint8_t* gainUpdates, uint16_t* outputValues);
+    void multiwrite(uint8_t numUpdates, uint8_t* channelSelect, uint16_t* outputValues);
 
-    void setLEDBrightness(uint8_t LED, uint8_t LEDBrightnessValue);
-    void setOutColor(uint8_t out, uint8_t outColorValue);
-    void setRGBColor(uint8_t LED, uint8_t valueR, uint8_t valueG, uint8_t valueB); //currently only works when auto-increment is enabled (is by default)
+    void sequentialWrite(uint8_t channelStart, uint8_t* channelUpdates, uint8_t* referenceUpdates, uint8_t* powerDownUpdates, uint8_t* gainUpdates, uint16_t* outputValues);
+    void sequentialWrite(uint8_t channelStart, uint16_t* outputValues);
+    void sequentialWriteEEPROM(uint8_t channelStart, uint8_t channelUpdate, uint8_t* referenceUpdates, uint8_t* powerDownUpdates, uint8_t* gainUpdates, uint16_t* outputValues);
+    
+    void fastWrite(uint8_t* powerDownUpdates, uint16_t* outputValues, uint8_t numChannelWrites);
+    void fastWrite(uint16_t* outputValues, uint8_t numChannelWrites);
+    void fastWrite(uint16_t outputChannel1, uint16_t outputChannel2, uint16_t outputChannel3, uint16_t outputChannel4);
 
-    void registerWrite(uint8_t reg, uint8_t value);
-    uint8_t registerRead(uint8_t reg);
+    void writeAddressBits();
+    void writeReferenceBits(uint8_t referenceConfig);
+    void writeGainBits(uint8_t gainConfig);
+    void writePowerDownBits(uint8_t powerDownConfig);
+    
+    void setLDAC(GPIO_PinState pinState);
+    GPIO_PinState getRDYState();
+
+    void genCall_reset();
+    void genCall_wakeUp();
+    void genCall_softwareUpdate();
+    void genCall_readAddrBits();
 
 private:
     I2C_HandleTypeDef* i2c_handle;
     uint8_t i2c_address;
-    uint8_t data_to_send;
-
+    GPIO_TypeDef* LDAC_pinPort;
+    uint16_t LDAC_pin;
+    GPIO_TypeDef* RDY_pinPort;
+    uint16_t RDY_pin;
+    
 };
 
 #endif
